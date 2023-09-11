942af1246f3f72f0b28cb2d735cedc6867da2f6c
Sun Jiao <P.ozzy@outlook.com>
2023-09-11 17:29:48 +0800

    misc updates on the insert test

--- test/rule/insert.dart
+++ test/rule/insert.dart
@@ -40,38 +40,28 @@ void main() {
 
   test('insert from end with fixed index', () async {
     String fileName = "SJ628901.ARW";
-    String insert = '-Bamtho-'; // string to be inserted
+    String insert = '-Bamtho'; // string to be inserted
     int index = 4; // insert before character at index
     bool fromStart = false; // true: count from start; false: from end.
     bool ignoreExtension = true;
 
-    String newFileName = RuleInsert(
+    final rule = RuleInsert(
       insert,
       index,
       fromStart,
       ignoreExtension,
-    ).newName(fileName);
+    );
 
-    expect(newFileName, "SJ62-Bamtho-8901.ARW");
+    String newFileName = rule.newName(fileName);
 
-    newFileName = RuleInsert(
-      'insert',
-      index,
-      fromStart,
-      ignoreExtension,
-    ).newName(newFileName);
+    expect(newFileName, "SJ62-Bamtho8901.ARW");
 
-    expect(newFileName, "SJ62-Bamtho-insert8901.ARW");
+    newFileName = rule.newName(newFileName);
 
+    expect(newFileName, "SJ62-Bamtho-Bamtho8901.ARW");
 
+    newFileName = rule.newName(newFileName);
 
-    newFileName = RuleInsert(
-      'insert2',
-      index,
-      fromStart,
-      ignoreExtension,
-    ).newName(newFileName);
-
-    expect(newFileName, "SJ62-Bamtho-insertinsert28901.ARW");
+    expect(newFileName, "SJ62-Bamtho-Bamtho-Bamtho8901.ARW");
   });
 }
