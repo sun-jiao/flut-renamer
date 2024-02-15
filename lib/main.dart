e07cf088b53826c6c17b1e4e8253950d350b150d
孙娇 <P.ozzy@outlook.com>
2024-02-15 14:14:12 +0800

    change theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:renamer/entity/theme_extension.dart';
-import 'package:yaru/yaru.dart';
 
 import 'entity/sharedpref.dart';
+import 'entity/theme_extension.dart';
 import 'pages/home_page.dart';
 
 Future<void> main() async {
@@ -20,50 +19,40 @@ class RenamerApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme.apply(
-      fontSizeFactor: 0.9,
-      fontSizeDelta: 0.9,
-    );
-    final listTileTheme = Theme.of(context).listTileTheme.copyWith(
-      dense: true,
-    );
-
-    return YaruTheme(
-      data: const YaruThemeData(
-        variant: YaruVariant.ubuntuButterflyPink,
+    return MaterialApp(
+      title: 'Renamer',
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.light,
+          seedColor: Colors.pinkAccent,
+        ),
+        brightness: Brightness.light,
+        useMaterial3: true,
+        extensions: <ThemeExtension<dynamic>>[
+          FileListColors(
+            primaryColor: Colors.white,
+            secondaryColor: Colors.grey.shade100,
+          ),
+        ],
       ),
-      builder: (context, yaru, child) {
-        return MaterialApp(
-          title: 'Renamer',
-          theme: yaru.theme?.copyWith(
-            textTheme: textTheme,
-            listTileTheme: listTileTheme,
-            checkboxTheme: yaru.theme?.checkboxTheme.copyWith(
-              fillColor: yaru.darkTheme?.checkboxTheme.fillColor,
-            ),
-            extensions: <ThemeExtension<dynamic>>[
-              FileListColors(
-                primaryColor: Colors.white,
-                secondaryColor: Colors.grey.shade100,
-              ),
-            ],
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.dark,
+          seedColor: Colors.pinkAccent,
+        ),
+        brightness: Brightness.dark,
+        useMaterial3: true,
+        extensions: <ThemeExtension<dynamic>>[
+          FileListColors(
+            primaryColor: Colors.grey.shade900,
+            secondaryColor: Colors.grey.shade800,
           ),
-          darkTheme: yaru.darkTheme?.copyWith(
-            textTheme: textTheme,
-            listTileTheme: listTileTheme,
-            checkboxTheme: yaru.darkTheme?.checkboxTheme.copyWith(
-              fillColor: yaru.theme?.checkboxTheme.fillColor,
-            ),
-            extensions: <ThemeExtension<dynamic>>[
-              FileListColors(
-                primaryColor: Colors.grey.shade900,
-                secondaryColor: Colors.grey.shade800,
-              ),
-            ],
-          ),
-          home: HomePage(),
-        );
-      },
+        ],
+      ),
+      home: SafeArea(
+        child: HomePage(),
+      ),
     );
   }
 }
--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   desktop_drop
-  gtk
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
