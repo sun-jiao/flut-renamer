7ac2f9f57b0bac0ede76d87d10e7351353244c15
孙娇 <P.ozzy@outlook.com>
2024-02-16 17:09:05 +0800

    change icon

--- appimage_build.sh
+++ appimage_build.sh
@@ -1,7 +1,7 @@
 flutter build linux --release
 cp -r ./build/linux/x64/release/bundle/ ./build/linux/x64/renamer.AppDir
 cp -r ./appimage/renamer.desktop ./build/linux/x64/renamer.AppDir
-cp -r ./assets/icon.png ./build/linux/x64/renamer.AppDir
+cp -r ./assets/ic_launcher.png ./build/linux/x64/renamer.AppDir
 cp -r ./appimage/AppRun ./build/linux/x64/renamer.AppDir
 chmod +x ./build/linux/x64/renamer.AppDir/AppRun
 appimagetool ./build/linux/x64/renamer.AppDir
