c675d7ea0b2e33737086d27b1ef0c93608c2d720
孙娇 <P.ozzy@outlook.com>
2024-02-25 01:12:09 +0800

    add appimage build

---
+++ github_appimage_build.sh
@@ -0,0 +1,15 @@
+sudo apt install -y python3-pip python3-setuptools patchelf desktop-file-utils libgdk-pixbuf2.0-dev fakeroot strace fuse
+
+# Install appimagetool AppImage
+sudo wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O /usr/local/bin/appimagetool
+sudo chmod +x /usr/local/bin/appimagetool
+
+flutter build linux --release
+cp -r ./build/linux/x64/release/bundle/ ./build/linux/x64/renamer.AppDir
+cp -r ./appimage/renamer.desktop ./build/linux/x64/renamer.AppDir
+cp -r ./assets/desktop.png ./build/linux/x64/renamer.AppDir
+cp -r ./appimage/AppRun ./build/linux/x64/renamer.AppDir
+chmod +x ./build/linux/x64/renamer.AppDir/AppRun
+appimagetool ./build/linux/x64/renamer.AppDir
+rm -rf ./build/linux/x64/renamer.AppDir
+cp Renamer-x86_64.AppImage ./build/linux/x64/release/bundle/
