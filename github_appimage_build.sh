06ab1b92df84334cecb41954c786a939b0e78965
孙娇 <P.ozzy@outlook.com>
2024-02-27 10:18:36 +0800

    remove redundant `flutter build linux`

--- github_appimage_build.sh
+++ github_appimage_build.sh
@@ -4,7 +4,6 @@ sudo apt install -y python3-pip python3-setuptools patchelf desktop-file-utils l
 sudo wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O /usr/local/bin/appimagetool
 sudo chmod +x /usr/local/bin/appimagetool
 
-flutter build linux --release
 cp -r ./build/linux/x64/release/bundle/ ./build/linux/x64/renamer.AppDir
 cp -r ./appimage/renamer.desktop ./build/linux/x64/renamer.AppDir
 cp -r ./assets/desktop.png ./build/linux/x64/renamer.AppDir
