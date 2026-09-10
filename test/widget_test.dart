db2ae505c85b7834e48593e5cc49e26ab63913b4
Sun Jiao <P.ozzy@outlook.com>
2026-09-10 14:05:32 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,17 +7,18 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
+import 'pages/files_page_test.dart' as files_page_test;
+import 'pages/home_tool_bar_test.dart' as home_tool_bar_test;
 import 'rule/replace.dart' as replace_test;
 import 'rule/insert.dart' as insert_test;
 import 'rule/increment.dart' as increment_test;
 import 'rule/rearrange.dart' as rearrange_test;
 import 'rule/rule_serialization_test.dart' as rule_serialization_test;
+import 'rule/random_string_test.dart' as random_string_test;
 import 'tools/rename_test.dart' as rename_test;
 import 'tools/file_metadata_test.dart' as file_metadata_test;
 import 'tools/file_sort_test.dart' as file_sort_test;
 import 'widget/custom_drop_test.dart' as custom_drop_test;
-import 'pages/files_page_test.dart' as files_page_test;
-import 'pages/home_tool_bar_test.dart' as home_tool_bar_test;
 
 void main() {
   group('rule replace', replace_test.main);
@@ -26,6 +27,7 @@ void main() {
   group('rule rearrange', rearrange_test.main);
   group('rule replace serialization', rule_serialization_test.main);
   group('rename test', rename_test.main);
+  group('random string test', random_string_test.main);
   group('file metadata test', file_metadata_test.main);
   group('file sort test', file_sort_test.main);
   group('custom drop test', custom_drop_test.main);
